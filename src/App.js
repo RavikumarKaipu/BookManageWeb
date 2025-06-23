d054957a77734ae8fa935569945a9cbfc9c84fb6
Master <sureshpb69@gmail.com>
2025-06-23 10:55:20 +0530

    Fix: Added JWT token to axios headers for PUT/POST requests

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
-import React, { useEffect } from 'react';
-import Cookies from 'js-cookie'
-=======
 import React, { useEffect, useState } from 'react';
 import Cookies from 'js-cookie';
->>>>>>> 8b55888 (Remove node_modules from repo)
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Home from './components/Home/Home';
@@ -16,20 +11,9 @@ import About from './components/About/About';
 import SearchResults from './components/SearchResults/SearchResults';
 import AddEditBook from './components/AddBook/AddBook';
 import ProtectedRoute from './components/ProtectedRoute';
-<<<<<<< HEAD
 import './App.css';
 import Signup from './components/SignUpPage/SignUp';
-
 import UsersList from './components/UsersList/UsersList'
-import LoginForm from './components/LoginForm';
-import { ThemeProvider } from './components/Themecontext/ThemeContext';
-import ForgotPassword from './components/ForgotPassword/ForgotPassword';
-
-
-const App = () => {
-=======
-import Signup from './components/SignUpPage/SignUp';
-import UsersList from './components/UsersList/UsersList';
 import LoginForm from './components/LoginForm';
 import { ThemeProvider } from './components/Themecontext/ThemeContext';
 import ForgotPassword from './components/ForgotPassword/ForgotPassword';
@@ -40,23 +24,14 @@ import NotFound from './components/NotFound';
 
 const App = () => {
   const [isOnline, setIsOnline] = useState(navigator.onLine);
-
->>>>>>> 8b55888 (Remove node_modules from repo)
   useEffect(() => {
     const updateTitle = () => {
       const jwt = Cookies.get('jwt_token');
       const title = jwt ? 'BookManagement' : 'Login';
       document.title = title;
     };
-<<<<<<< HEAD
     updateTitle();
     const interval = setInterval(updateTitle, 500); 
-    return () => clearInterval(interval);
-  }, []);
-=======
-
-    updateTitle();
-    const interval = setInterval(updateTitle, 500);
     return () => clearInterval(interval);
   }, []);
 
@@ -76,26 +51,22 @@ const App = () => {
     return <NoConnection />;
   }
 
->>>>>>> 8b55888 (Remove node_modules from repo)
   return (
     <ThemeProvider>
       <Router>
         <Routes>
-<<<<<<< HEAD
           <Route path="/signup" element={<Signup/>}/>
           <Route path='/forgotPassword' element={<ForgotPassword/>}/>
           <Route path="/" element={<ProtectedRoute element={<Home />} />} />
           <Route exact path="/login" element={<LoginForm/>} />
           <Route path="/books-list" element={<ProtectedRoute element={<BooksList />} />} />
           <Route path="/book/:id" element={<ProtectedRoute element={<BookDetails />} />} />
-=======
           <Route path="/signup" element={<Signup />} />
           <Route path="/forgotPassword" element={<ForgotPassword />} />
           <Route path="/" element={<ProtectedRoute element={<Home />} />} />
           <Route path="/login" element={<LoginForm />} />
           <Route path="/books-list" element={<ProtectedRoute element={<BooksList />} />} />
           <Route path="/books/:id" element={<ProtectedRoute element={<BookDetails />} />} />
->>>>>>> 8b55888 (Remove node_modules from repo)
           <Route path="/add-book" element={<ProtectedRoute element={<AddBook />} />} />
           <Route path="/edit/:id" element={<ProtectedRoute element={<AddEditBook />} />} />
           <Route path="/contact" element={<ProtectedRoute element={<Contact />} />} />
@@ -103,10 +74,7 @@ const App = () => {
           <Route path="/search-results" element={<ProtectedRoute element={<SearchResults />} />} />
           <Route path="/book-details/:id" element={<ProtectedRoute element={<BookDetails />} />} />
           <Route path="/users-list" element={<ProtectedRoute element={<UsersList />} />} />
-<<<<<<< HEAD
-=======
           <Route path='*' element={<NotFound/>}/>
->>>>>>> 8b55888 (Remove node_modules from repo)
         </Routes>
       </Router>
     </ThemeProvider>
--- src/components/AddBook/AddBook.js
+++ src/components/AddBook/AddBook.js
@@ -1,6 +1,6 @@
-<<<<<<< HEAD
 import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
+import Cookies from 'js-cookie'
 import axios from 'axios';
 import './AddBook.css';
 
@@ -16,9 +16,8 @@ const AddEditBook = () => {
     PublishedDate: ''
   });
 
-  // Fetch the existing book details if editing
   useEffect(() => {
-    const token = localStorage.getItem('token'); // Assuming you stored the token in localStorage
+    const token = Cookies.get('jwt_token'); // Assuming you stored the token in localStorage
 
     if (id) {
       axios.get(`http://localhost:5000/books/${id}`, {
@@ -43,7 +42,7 @@ const AddEditBook = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    
+    const token = Cookies.get('jwt_token');
     const method = id ? 'put' : 'post';
     const url = id ? `http://localhost:5000/books/${id}` : 'http://localhost:5000/books';
     
@@ -110,117 +109,3 @@ const AddEditBook = () => {
 };
 
 export default AddEditBook;
-=======
-import React, { useState, useEffect } from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
-import Cookies from 'js-cookie'
-import axios from 'axios';
-import './AddBook.css';
-
-const AddEditBook = () => {
-  const { id } = useParams();
-  const navigate = useNavigate();
-
-  const [book, setBook] = useState({
-    Title: '',
-    Author: '',
-    Genre: '',
-    Pages: '',
-    PublishedDate: ''
-  });
-
-  // Fetch the existing book details if editing
-  useEffect(() => {
-    const token = Cookies.get('jwt_token');
-
-    if (id) {
-      axios.get(`https://bookmanage-backend.vercel.app/books/${id}`, {
-        headers: {
-          'Authorization': `Bearer ${token}`, // Include token in request header
-        }
-      })
-        .then(response => {
-          setBook(response.data); 
-        })
-        .catch(error => {
-          console.error('Error fetching book:', error);
-        });
-    }
-  }, [id]);
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setBook({ ...book, [name]: value });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    
-    const method = id ? 'put' : 'post';
-    const url = id ? `https://bookmanage-backend.vercel.app/books/${id}` : 'https://bookmanage-backend.vercel.app/books';
-    
-    axios[method](url, book)
-      .then(response => {
-        navigate('/', { replace: true });
-        alert('Book saved');
-      })
-      .catch(error => {
-        alert('Error saving book, please try again.');
-      });
-  };
-
-  return (
-    <div className="add-edit-book">
-      <h2>{id ? 'Edit Book' : 'Add New Book'}</h2>
-      <form onSubmit={handleSubmit}>
-        <input 
-          type="text" 
-          name="Title" 
-          value={book.Title} 
-          onChange={handleChange} 
-          placeholder="Title" 
-          required 
-        />
-        <input 
-          type="text" 
-          name="Author" 
-          value={book.Author} 
-          onChange={handleChange} 
-          placeholder="Author" 
-          required 
-        />
-        <input 
-          type="text" 
-          name="Genre" 
-          value={book.Genre} 
-          onChange={handleChange} 
-          placeholder="Genre" 
-          required 
-        />
-        <input 
-          type="number" 
-          name="Pages" 
-          value={book.Pages} 
-          onChange={handleChange} 
-          placeholder="Pages" 
-          min="0" 
-          required 
-        />
-        <input 
-          type="date" 
-          name="PublishedDate" 
-          value={book.PublishedDate} 
-          onChange={handleChange} 
-          required 
-        />
-        <button type="submit">{id ? 'Update Book' : 'Save Book'}</button>
-      </form>
-      <h4 style={{textAlign: "center"}}>or</h4>
-      <button onClick={() => navigate('/', { replace: true })}>Back to Home</button>
-    </div>
-  );
-};
-
-export default AddEditBook;
->>>>>>> 8b55888 (Remove node_modules from repo)
